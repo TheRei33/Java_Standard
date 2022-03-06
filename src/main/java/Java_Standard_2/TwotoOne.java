7ad49428269752b0a7459439e663edfd83fcbda7
Jeong_hyunwoo <rlck51@naver.com>
2022-03-06 16:10:04 +0900

    Initial commit

---
+++ src/main/java/Java_Standard_2/TwotoOne.java
@@ -0,0 +1,15 @@
+package Java_Standard_2;
+
+public class TwotoOne {
+    public static void main(String[] args) {
+        int x =10,y=20;
+        int tmp = 0;
+
+        System.out.println("X: "+x+"Y: "+y);
+
+        tmp =x;
+        x=y;
+        y=tmp;
+        System.out.println("X: "+x+"Y: "+y);
+    }
+}
