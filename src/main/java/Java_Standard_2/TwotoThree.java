cae43c6df60a5f14acbd50ab2789487fc43d2d78
Jeong_hyunwoo <rlck51@naver.com>
2022-03-08 11:39:22 +0900

    자바의 정석 예제 2-3

---
+++ src/main/java/Java_Standard_2/TwotoThree.java
@@ -0,0 +1,20 @@
+package Java_Standard_2;
+
+public class TwotoThree {
+    public static void main(String[] args) {
+        String name = "ja" + "va";
+        String str = name + 8.0;
+        System.out.println(name);
+        System.out.println(str);
+        System.out.println(7 + " ");
+        System.out.println(" " + 7);
+        System.out.println(7 + "");
+        System.out.println("" + 7);
+        System.out.println("" + "");
+        System.out.println(7 + 7 + "");
+        System.out.println("" + 7 + 7);
+
+        // 문자열으로 변활때는 (""+)으로 시작
+
+    }
+}
