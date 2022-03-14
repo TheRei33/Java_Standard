8ede5246bc853b82eda8db835fb8005c7a32f189
Jeong_hyunwoo <rlck51@naver.com>
2022-03-14 09:52:16 +0900

    자바의 정석 예제 2-8

---
+++ src/main/java/Java_Standard_2/TwotoEight.java
@@ -0,0 +1,12 @@
+package Java_Standard_2;
+
+public class TwotoEight {
+    public static void main(String[] args) {
+        System.out.println('\'');
+        System.out.println("abc\t123\b456");
+        System.out.println('\n');
+        System.out.println("\"Hello\"");
+        System.out.println("c:\\");
+
+    }
+}
