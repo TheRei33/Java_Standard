b9c05b5d044f08bda9f869b0a04bfe34b6befa9c
Jeong_hyunwoo <rlck51@naver.com>
2022-03-16 07:28:22 +0900

    자바의 정석 예제 2-10

---
+++ src/main/java/Java_Standard_2/TwotoTen.java
@@ -0,0 +1,15 @@
+package Java_Standard_2;
+
+public class TwotoTen {
+    public static void main(String[] args) {
+        float f = 9.12345678901234567890f;
+        float f2 = 1.2345678901234567890f;
+        double d =9.12345678901234567890d;
+
+        System.out.printf("      12345678901234567890123%n");
+        System.out.printf("f  : %f%n",f);
+        System.out.printf("f  : %24.20f%n",f);
+        System.out.printf("f2 : %24.20f%n",f2);
+        System.out.printf("d  : %24.20f%n",d);
+    }
+}
