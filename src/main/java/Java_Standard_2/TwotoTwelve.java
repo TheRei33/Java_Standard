e1503cff1d7d2b66bd8aec9d2a606040c7fa0b30
Jeong_hyunwoo <rlck51@naver.com>
2022-03-18 10:06:33 +0900

    자바의 정석 예제 2-12

---
+++ src/main/java/Java_Standard_2/TwotoTwelve.java
@@ -0,0 +1,11 @@
+package Java_Standard_2;
+
+public class TwotoTwelve {
+    public static void main(String[] args) {
+        double d = 85.4;
+        int score = (int)d;
+
+        System.out.println("score = "+ score);
+        System.out.println("d = "+d);
+    }
+}
