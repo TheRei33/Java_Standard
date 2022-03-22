7dd54b08cfd4b3f6b8cecda28bbf0057f8da244b
Jeong_hyunwoo <rlck51@naver.com>
2022-03-23 07:33:48 +0900

    자바의 정석 예제 3-1

---
+++ src/main/java/Java_Standard_3/ThreetoOne.java
@@ -0,0 +1,13 @@
+package Java_Standard_3;
+
+public class ThreetoOne {
+    public static void main(String[] args) {
+        int i = 5;
+        i++;
+        System.out.println(i);
+
+        i=5;
+        ++i;
+        System.out.println(i);
+    }
+}
