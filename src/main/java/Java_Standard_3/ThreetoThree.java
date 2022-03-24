35b09c328e639104723b4a448110a27f58412812
Jeong_hyunwoo <rlck51@naver.com>
2022-03-25 08:35:23 +0900

    자바의 정석 예제 3-3

---
+++ src/main/java/Java_Standard_3/ThreetoThree.java
@@ -0,0 +1,10 @@
+package Java_Standard_3;
+
+public class ThreetoThree {
+    public static void main(String[] args) {
+        int i = 5, j = 5;
+        System.out.println(i++);
+        System.out.println(++j);
+        System.out.println("i = "+i+", j = "+j);
+    }
+}
