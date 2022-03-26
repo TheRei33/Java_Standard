3b8fc7d51db5b0c0f037de64a2a077089b49720e
Jeong_hyunwoo <rlck51@naver.com>
2022-03-26 19:48:39 +0900

    자바의 정석 예제 3-4

---
+++ src/main/java/Java_Standard_3/ThreetoFour.java
@@ -0,0 +1,13 @@
+package Java_Standard_3;
+
+public class ThreetoFour {
+    public static void main(String[] args) {
+        int i = -10;
+        i = +i;
+        System.out.println(i);
+
+        i = -10;
+        i = -i;
+        System.out.println(i);
+    }
+}
