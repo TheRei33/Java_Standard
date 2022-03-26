be2afdd0e4161ce65975970fd629c606d3e2367d
Jeong_hyunwoo <rlck51@naver.com>
2022-03-27 08:14:25 +0900

    자바의 정석 예제 3-5

---
+++ src/main/java/Java_Standard_3/ThreetoFive.java
@@ -0,0 +1,14 @@
+package Java_Standard_3;
+
+public class ThreetoFive {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+
+        System.out.printf("%d + %d = %d%n", a, b, a+b);
+        System.out.printf("%d - %d = %d%n", a, b, a-b);
+        System.out.printf("%d * %d = %d%n", a, b, a*b);
+        System.out.printf("%d / %d = %d%n", a, b, a/b);
+        System.out.printf("%d / %f = %f%n", a, (float)b, a/(float)b);
+    }
+}
