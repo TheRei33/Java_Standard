d434d822ceb1e5db6c72ae7152ee9e38a3ade0ff
Jeong_hyunwoo <rlck51@naver.com>
2022-03-28 09:34:26 +0900

    자바의 정석 예제 3-6

---
+++ src/main/java/Java_Standard_3/ThreetoSix.java
@@ -0,0 +1,10 @@
+package Java_Standard_3;
+
+public class ThreetoSix {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+//      byte c =  a + b ;
+//      System.out.println(c);
+    }
+}
