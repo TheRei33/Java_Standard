d2012b6edeaebb517d6610dfe620c492852cee3e
Jeong_hyunwoo <rlck51@naver.com>
2022-03-30 07:44:32 +0900

    자바의 정석 예제 3-8

---
+++ src/main/java/Java_Standard_3/ThreetoEight.java
@@ -0,0 +1,12 @@
+package Java_Standard_3;
+
+public class ThreetoEight {
+    public static void main(String[] args) {
+        int a = 1_000_000;
+        int b = 2_000_000;
+
+        long c = a * b;
+
+        System.out.println(c);
+    }
+}
