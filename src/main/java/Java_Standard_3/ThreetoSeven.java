fef9222d4e8d1d33b770fa9f2dc4d837b501de8e
Jeong_hyunwoo <rlck51@naver.com>
2022-03-29 09:57:51 +0900

    자바의 정석 예제 3-7

---
+++ src/main/java/Java_Standard_3/ThreetoSeven.java
@@ -0,0 +1,11 @@
+package Java_Standard_3;
+
+public class ThreetoSeven {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 30;
+        byte c = (byte)( a * b ) ;
+        System.out.println(c);
+
+    }
+}
