8211c7056da17a01d16979dd1dcbd34844230d62
Jeong_hyunwoo <rlck51@naver.com>
2022-03-31 09:05:31 +0900

    자바의 정석 예제 3-9

---
+++ src/main/java/Java_Standard_3/ThreetoNine.java
@@ -0,0 +1,11 @@
+package Java_Standard_3;
+
+public class ThreetoNine {
+    public static void main(String[] args) {
+        long a = 1_000_000 * 1_000_000;
+        long b = 1_000_000 * 1_000_000L;
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
+}
