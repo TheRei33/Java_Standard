964535a719df57ffa7b3a02461001b9a1551d5d8
Jeong_hyunwoo <rlck51@naver.com>
2022-04-02 15:52:29 +0900

    자바의 정석 예제 3-11

---
+++ src/main/java/Java_Standard_3/ThreetoEleven.java
@@ -0,0 +1,17 @@
+package Java_Standard_3;
+
+public class ThreetoEleven {
+    public static void main(String[] args) {
+        char a = 'a';
+        char d = 'd';
+        char zero = '0';
+        char two = '2';
+
+        System.out.printf("'%c' - '%c' = %d%n", d, a, d - a);
+        System.out.printf("'%c' - '%c' = %d%n", two, zero, two - zero);
+        System.out.printf("'%c'= %d%n ", a, (int)a);
+        System.out.printf("'%c'= %d%n ", d, (int)d);
+        System.out.printf("'%c'= %d%n ", zero, (int)zero);
+        System.out.printf("'%c'= %d%n ", two, (int)two);
+    }
+}
