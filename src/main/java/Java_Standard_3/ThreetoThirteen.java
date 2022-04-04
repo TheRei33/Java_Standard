b36b33c5892b67a01b9f65c2144607cdbbe358f9
Jeong_hyunwoo <rlck51@naver.com>
2022-04-04 10:13:41 +0900

    자바의 정석 예제 3-13

---
+++ src/main/java/Java_Standard_3/ThreetoThirteen.java
@@ -0,0 +1,13 @@
+package Java_Standard_3;
+
+public class ThreetoThirteen {
+    public static void main(String[] args) {
+        char c1 = 'a';
+
+//        char c2 = c1+1;
+        char c2 = 'a'+1;
+
+        System.out.println(c2);
+
+    }
+}
