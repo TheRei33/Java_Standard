3a0f0983469c921a8084234b8f130193c4017420
Jeong_hyunwoo <rlck51@naver.com>
2022-04-06 13:33:41 +0900

    자바의 정석 예제 3-15

---
+++ src/main/java/Java_Standard_3/ThreetoFifteen.java
@@ -0,0 +1,9 @@
+package Java_Standard_3;
+
+public class ThreetoFifteen {
+    public static void main(String[] args) {
+        char lowerCase = 'a';
+        char upperCase = (char)(lowerCase - 32);
+        System.out.println(upperCase);
+    }
+}
