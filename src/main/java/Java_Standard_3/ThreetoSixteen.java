d033232c8bfdbc1f47082ee275f091bedcc6b34c
Jeong_hyunwoo <rlck51@naver.com>
2022-04-07 10:18:33 +0900

    자바의 정석 예제 3-16

---
+++ src/main/java/Java_Standard_3/ThreetoSixteen.java
@@ -0,0 +1,9 @@
+package Java_Standard_3;
+
+public class ThreetoSixteen {
+    public static void main(String[] args) {
+        float pi = 3.141592f;
+        float shortPi = (int)(pi * 1000) / 1000f;
+        System.out.println(shortPi);
+    }
+}
