f539572aca7311fa1ba13f6e82794523948429ea
Jeong_hyunwoo <rlck51@naver.com>
2022-04-09 10:42:39 +0900

    자바의 정석 예제 3-18

---
+++ src/main/java/Java_Standard_3/ThreetoEighteen.java
@@ -0,0 +1,11 @@
+package Java_Standard_3;
+
+public class ThreetoEighteen {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            double pi = 3.141592;
+            double shortPi = Math.round(pi * 1000) / 1000.0;
+            System.out.println(shortPi);
+        }
+    }
+}
