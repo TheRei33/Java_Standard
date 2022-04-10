ef0bc510fa1d315ffc226472c20aeb4f497aff36
Jeong_hyunwoo <rlck51@naver.com>
2022-04-10 09:35:18 +0900

    자바의 정석 예제 3-19

---
+++ src/main/java/Java_Standard_3/ThreetoNineteen.java
@@ -0,0 +1,11 @@
+package Java_Standard_3;
+
+public class ThreetoNineteen {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 8;
+
+        System.out.printf("%d을 %d로 나누면, %n", x, y);
+        System.out.printf("몫은 %d이고, 나머지는 %d입니다", x / y , x % y);
+    }
+}
