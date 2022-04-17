242bf915a5152f8a0b1f3b20684c6492e6dff9e3
Jeong_hyunwoo <rlck51@naver.com>
2022-04-17 09:56:34 +0900

    자바의 정석 예제 3-26

---
+++ src/main/java/Java_Standard_3/ThreetoTwentysix.java
@@ -0,0 +1,14 @@
+package Java_Standard_3;
+
+public class ThreetoTwentysix {
+    public static void main(String[] args) {
+        int a = 5 ;
+        int b = 0 ;
+
+        System.out.printf("a = %d, b = %d%n",a,b);
+        System.out.printf("a != 0 || ++b !=0 = %b%n",a != 0 || ++b != 0);
+        System.out.printf("a = %d, b = %d%n",a,b);
+        System.out.printf("a == 0 && ++b != 0 = %b%n",a == 0 && ++b !=0);
+        System.out.printf("a = %d, b = %d%n",a,b);
+    }
+}
