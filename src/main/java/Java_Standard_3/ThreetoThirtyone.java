79c09c3903b32ddda9e16bac7f51273dae537c26
Jeong_hyunwoo <rlck51@naver.com>
2022-04-22 07:40:07 +0900

    자바의 정석 예제 3-31

---
+++ src/main/java/Java_Standard_3/ThreetoThirtyone.java
@@ -0,0 +1,21 @@
+package Java_Standard_3;
+
+public class ThreetoThirtyone {
+    public static void main(String[] args) {
+        int dec = 1234 ;
+        int hex = 0xABCD ;
+        int mask = 0xF ;
+
+        System.out.printf("hex = %X%n", hex);
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4 ;
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4 ;
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4 ;
+        System.out.printf("%X%n", hex & mask);
+    }
+}
