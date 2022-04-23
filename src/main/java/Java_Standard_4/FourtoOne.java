dcbb85fec86db7cc5d0052acadaf381246850f82
Jeong_hyunwoo <rlck51@naver.com>
2022-04-24 08:44:21 +0900

    자바의 정석 예제 4-1

---
+++ src/main/java/Java_Standard_4/FourtoOne.java
@@ -0,0 +1,21 @@
+package Java_Standard_4;
+
+public class FourtoOne {
+    public static void main(String[] args) {
+        int x = 0 ;
+        System.out.printf("x = %d 일때, 참인 것은 %n", x);
+
+        if( x == 0 ) System.out.println("x == 0");
+        if( x != 0 ) System.out.println("x != 0");
+        if( !( x == 0 ) ) System.out.println("!( x == 0 )");
+        if( !( x != 0 ) ) System.out.println("!( x != 0 )");
+
+        x = 1;
+        System.out.printf("x = %d 일때, 참인 것은 %n", x);
+
+        if( x == 0 ) System.out.println("x == 0");
+        if( x != 0 ) System.out.println("x != 0");
+        if( !( x == 0 ) ) System.out.println("!( x == 0 )");
+        if( !( x != 0 ) ) System.out.println("!( x != 0 )");
+    }
+}
