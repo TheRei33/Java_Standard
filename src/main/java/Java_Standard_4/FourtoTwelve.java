bdb062e33e7d57702b911ec5ccd06e646fce370a
Jeong_hyunwoo <rlck51@naver.com>
2022-05-05 08:31:52 +0900

    자바의 정석 예제 4-12

---
+++ src/main/java/Java_Standard_4/FourtoTwelve.java
@@ -0,0 +1,13 @@
+package Java_Standard_4;
+
+public class FourtoTwelve {
+    public static void main(String[] args) {
+        for( int i = 1 ; i <= 5 ; i++) {
+            System.out.println(i);
+        }
+        for( int i = 1 ; i <= 5 ; i++ ) {
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+}
