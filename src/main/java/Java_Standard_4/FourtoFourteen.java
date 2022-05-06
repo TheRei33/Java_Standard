5d0cfbe189f6e0b02aff813b8e06b60d7c9c4c2a
Jeong_hyunwoo <rlck51@naver.com>
2022-05-07 08:10:45 +0900

    자바의 정석 예제 4-14

---
+++ src/main/java/Java_Standard_4/FourtoFourteen.java
@@ -0,0 +1,9 @@
+package Java_Standard_4;
+
+public class FourtoFourteen {
+    public static void main(String[] args) {
+        for( int i = 1, j = 10 ; i <= 10 ; i++ ,j--) {
+            System.out.printf("%d \t %d%n", i ,j );
+        }
+    }
+}
