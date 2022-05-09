c66f07d491b73b2f05379fd1231fa908e0bcc1f1
Jeong_hyunwoo <rlck51@naver.com>
2022-05-09 14:53:29 +0900

    자바의 정석 예제 4-16

---
+++ src/main/java/Java_Standard_4/FourtoSixteen.java
@@ -0,0 +1,13 @@
+package Java_Standard_4;
+
+public class FourtoSixteen {
+    public static void main(String[] args) {
+
+        for( int i = 1 ; i <= 5 ; i++) {
+            for( int j = 1 ; j <= 10 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
