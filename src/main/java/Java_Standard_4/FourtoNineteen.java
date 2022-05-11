9b89ac4a8a81d744cb13c04cb9d2cad6670dd04c
Jeong_hyunwoo <rlck51@naver.com>
2022-05-12 08:08:24 +0900

    자바의 정석 예제 4-19

---
+++ src/main/java/Java_Standard_4/FourtoNineteen.java
@@ -0,0 +1,13 @@
+package Java_Standard_4;
+
+public class FourtoNineteen {
+    public static void main(String[] args) {
+        for( int i = 1 ; i <= 3 ; i++) {
+            for( int j = 1 ; j <= 3 ; j++) {
+                for( int k = 1 ; k <= 3 ; k++ ) {
+                    System.out.println(""+i+j+k);
+                }
+            }
+        }
+    }
+}
