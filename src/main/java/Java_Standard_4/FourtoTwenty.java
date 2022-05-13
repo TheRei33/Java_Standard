099464ce65161ff15c89d854d726093b952e4ef6
Jeong_hyunwoo <rlck51@naver.com>
2022-05-13 09:41:16 +0900

    자바의 정석 예제 4-20

---
+++ src/main/java/Java_Standard_4/FourtoTwenty.java
@@ -0,0 +1,12 @@
+package Java_Standard_4;
+
+public class FourtoTwenty {
+    public static void main(String[] args) {
+        for( int i = 1 ; i <= 5 ; i++) {
+            for( int j = 1 ; j <= 5 ; j++) {
+                System.out.printf("[%d,%d]", i, j);
+            }
+            System.out.println();
+        }
+    }
+}
