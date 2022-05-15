1cca3e2105916d5108754fa4a2d60bc4e3091092
Jeong_hyunwoo <rlck51@naver.com>
2022-05-16 08:04:14 +0900

    자바의 정석 예제 4-23

---
+++ src/main/java/Java_Standard_4/FourtoTwentythree.java
@@ -0,0 +1,11 @@
+package Java_Standard_4;
+
+public class FourtoTwentythree {
+    public static void main(String[] args) {
+        int i = 5 ;
+
+        while( i-- != 0 ) {
+            System.out.println( i + " - I can do it.");
+        }
+    }
+}
