36e1f5c6074f8fb695ad8de681a9f70b2ab64f38
Jeong_hyunwoo <rlck51@naver.com>
2022-05-24 09:37:45 +0900

    자바의 정석 예제 4-31

---
+++ src/main/java/Java_Standard_4/FourtoThirtyone.java
@@ -0,0 +1,11 @@
+package Java_Standard_4;
+
+public class FourtoThirtyone {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 10; i++) {
+            if(i%3 == 0)
+                continue;
+            System.out.println(i);
+        }
+    }
+}
