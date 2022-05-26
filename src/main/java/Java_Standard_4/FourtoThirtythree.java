b609f1e89a7d8176157d787e0e722b0b2906125c
Jeong_hyunwoo <rlck51@naver.com>
2022-05-26 13:16:31 +0900

    자바의 정석 예제 4-33

---
+++ src/main/java/Java_Standard_4/FourtoThirtythree.java
@@ -0,0 +1,18 @@
+package Java_Standard_4;
+
+public class FourtoThirtythree {
+    public static void main(String[] arg) {
+
+        Loop1 : for (int i =2; i <= 9;i++) {
+            for(int j = 1; j <=9; j++) {
+                if(j==5)
+                    break Loop1;
+//                    break;
+//                    continue Loop1;
+//                    continue;
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+            System.out.println();
+        }
+    }
+}
