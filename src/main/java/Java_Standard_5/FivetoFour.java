141a731d6d84363246efed65ea39f06af9b008b0
Jeong_hyunwoo <rlck51@naver.com>
2022-05-31 08:34:21 +0900

    자바의 정석 예제 5-4

---
+++ src/main/java/Java_Standard_5/FivetoFour.java
@@ -0,0 +1,23 @@
+package Java_Standard_5;
+
+public class FivetoFour {
+    public static void main(String[] args) {
+        char[] abc = { 'A', 'B', 'C', 'D'};
+        char[] num = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+        System.out.println(abc);
+        System.out.println(num);
+
+        char[] result = new char[abc.length+num.length];
+        System.arraycopy(abc, 0, result, 0, abc.length);
+        System.arraycopy(num, 0, result, abc.length, num.length);
+        System.out.println(result);
+
+        System.arraycopy(abc, 0, num, 0, abc.length);
+        System.out.println(num);
+
+        System.arraycopy(abc, 0, num, 6, 3);
+        System.out.println(num);
+    }
+
+}
