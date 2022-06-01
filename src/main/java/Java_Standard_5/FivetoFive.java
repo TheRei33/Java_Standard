305a43ba0309ce063a6b770206ee745b0912a098
Jeong_hyunwoo <rlck51@naver.com>
2022-06-01 09:01:11 +0900

    자바의 정석 예제 5-5

---
+++ src/main/java/Java_Standard_5/FivetoFive.java
@@ -0,0 +1,19 @@
+package Java_Standard_5;
+
+public class FivetoFive {
+    public static void main(String[] args) {
+        int sum = 0 ;
+        float average = 0f ;
+
+        int[] score = { 100, 88, 100, 100, 90 };
+
+        for( int i = 0 ; i < score.length ; i++ ) {
+            sum += score[i] ;
+        }
+
+        average = sum / (float) score.length ;
+
+        System.out.println("total score : " + sum);
+        System.out.println("average : " + average);
+    }
+}
