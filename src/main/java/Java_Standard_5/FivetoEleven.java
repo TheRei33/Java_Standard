8aa8d091b6cff007972e10cdeb63ad087469a27c
Jeong_hyunwoo <rlck51@naver.com>
2022-06-07 10:06:13 +0900

    자바의 정석 예제 5-11

---
+++ src/main/java/Java_Standard_5/FivetoEleven.java
@@ -0,0 +1,22 @@
+package Java_Standard_5;
+
+public class FivetoEleven {
+    public static void main(String[] args) {
+        int[] numArr =  new int[10] ;
+        int[] counter = new int[10] ;
+
+        for( int i = 0 ; i < numArr.length ; i++ ) {
+            numArr[i] = ( int )( Math.random() * 10 ) ;
+            System.out.print(numArr[i]);
+        }
+
+        System.out.println();
+
+        for( int i = 0 ; i < numArr.length  ; i++)
+            counter[numArr[i]]++;
+
+        for( int i = 0 ; i < numArr.length  ; i++ ) {
+            System.out.println( i + "의 개수 : " + counter[i] );
+        }
+    }  //  main
+}
