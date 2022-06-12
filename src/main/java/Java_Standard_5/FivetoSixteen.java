43e547ec4925dbccf8e483422a1a9289493e54f3
Jeong_hyunwoo <rlck51@naver.com>
2022-06-12 10:57:20 +0900

    자바의 정석 예제 5-16

---
+++ src/main/java/Java_Standard_5/FivetoSixteen.java
@@ -0,0 +1,11 @@
+package Java_Standard_5;
+
+public class FivetoSixteen {
+    public static void main(String[] args) {
+        System.out.println("매개변수의 개수 : "+args.length);
+        for( int i = 0 ; i < args.length ; i++ ) {
+            System.out.println("args[" + i + "] = \"" + args[i] + "\"");
+        } // for
+    }
+
+}
