ee5d87a4797f54f37d514d8cbbbb6b3ee0b2ef94
Jeong_hyunwoo <rlck51@naver.com>
2022-06-14 07:59:31 +0900

    자바의 정석 예제 5-18

---
+++ src/main/java/Java_Standard_5/FivetoEighteen.java
@@ -0,0 +1,28 @@
+package Java_Standard_5;
+
+public class FivetoEighteen {
+    public static void main(String[] args) {
+        int[][] score = {
+                {100, 100, 100},
+                {20, 20, 20},
+                {30, 30, 30},
+                {40, 40, 40}
+        };
+
+        int sum = 0;
+
+        for(int i = 0 ; i < score.length ; i++) {
+            for(int j = 0 ; j < score[i].length ; j++) {
+                System.out.printf("score[%d][%d] = %d%n", i, j, score[i][j]);
+            }  // for( j )
+        }  //for( i )
+
+        for(int[] tmp : score) {
+            for(int i : tmp) {
+                sum += i;
+            }
+        }
+
+        System.out.println("sum = "+sum);
+    }  //main
+}
