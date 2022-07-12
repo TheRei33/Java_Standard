a0e7c7a32e2ca2c5ddfb832c605fb450a7ac4c4f
Jeong_hyunwoo <rlck51@naver.com>
2022-07-12 10:46:24 +0900

    자바의 정석 예제 6-23

---
+++ src/main/java/Java_Standard_6/SixtoTwentythree.java
@@ -0,0 +1,21 @@
+package Java_Standard_6;
+
+class Data1{
+    int value;
+}
+
+class Data2{
+    int value;
+
+    Data2(int x){
+        value = x;
+    }
+}
+
+public class SixtoTwentythree {
+    public static void main(String[] args) {
+        Data1 d1 = new Data1();
+        Data2 d2 = new Data2();
+    }
+
+}
