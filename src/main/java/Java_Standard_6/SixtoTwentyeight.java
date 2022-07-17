7de591daac59b96e90a3c4df3a0222430691889f
Jeong_hyunwoo <rlck51@naver.com>
2022-07-17 18:28:55 +0900

    자바의 정석 예제 6-28

---
+++ src/main/java/Java_Standard_6/SixtoTwentyeight.java
@@ -0,0 +1,18 @@
+package Java_Standard_6;
+
+public class SixtoTwentyeight {
+    static int[] arr = new int[10];
+
+    static {
+        for( int i = 0 ; i < arr.length ; i++ ) {
+            arr[i] = (int) (Math.random()*10)+1;
+        }
+    }
+
+    public static void main(String[] args) {
+        for( int i = 0 ; i < arr.length ; i++ ) {
+            System.out.println("arr["+i+"] : "+arr[i]);
+        }
+    }
+
+}
