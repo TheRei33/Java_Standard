bc63a02e2aadacdc7b9d054cad88a46df3d1f688
Jeong_hyunwoo <rlck51@naver.com>
2022-07-19 12:09:52 +0900

    자바의 정석 예제 6-30

---
+++ src/main/java/Java_Standard_6/SixtoThirty.java
@@ -0,0 +1,27 @@
+package Java_Standard_6;
+
+class Document{
+    static int count = 0;
+    String name;
+
+    Document() {
+        this("제목없음"+ ++count);
+    }
+
+    public Document(String name) {
+        this.name = name;
+        System.out.println("문서 "+this.name + "가 생성되었습니다.");
+    }
+
+
+}
+
+public class SixtoThirty {
+    public static void main(String[] args) {
+        Document d1 = new Document();
+        Document d2 = new Document("자바.txt");
+        Document d3 = new Document();
+        Document d4 = new Document();
+    }
+
+}
