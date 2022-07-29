9e551a3092080036f0b6b23a6872ca0105d0d70f
Jeong_hyunwoo <rlck51@naver.com>
2022-07-30 08:29:04 +0900

    자바의 정석 예제 7-11

---
+++ src/main/java/Java_Standard_7/SeventoEleven.java
@@ -0,0 +1,15 @@
+package Java_Standard_7;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.random;
+import static java.lang.System.out;
+
+public class SeventoEleven {
+    public static void main(String[] args) {
+        // System.out.println( Math.random() );
+        out.println(random());
+
+        // System.out.println( "Math.PI : " + Math.PI );
+        out.println("Math.PI : " + PI);
+    }
+}
