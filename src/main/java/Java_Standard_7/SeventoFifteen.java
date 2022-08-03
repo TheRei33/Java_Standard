7d8efb0dc18c880529377648e3cabdf640aff5a7
Jeong_hyunwoo <rlck51@naver.com>
2022-08-03 15:20:23 +0900

    자바의 정석 예제 7-15

---
+++ src/main/java/Java_Standard_7/SeventoFifteen.java
@@ -0,0 +1,34 @@
+package Java_Standard_7;
+
+public class SeventoFifteen {
+    public static void main(String[] args) {
+        Car car = null;
+        FireEngine fe = new FireEngine();
+        FireEngine fe2 = null;
+
+        fe.water();
+        car = fe;
+//        car.water();
+        fe2 = (FireEngine) car;
+        fe2.water();
+    }
+}
+
+class Car{
+    String color;
+    int door;
+
+    void drive() {
+        System.out.println("drive, Brrrr~");
+    }
+
+    void stop() {
+        System.out.println("stop!!!");
+    }
+}
+
+class FireEngine extends Car{
+    void water() {
+        System.out.println("water!!!");
+    }
+}
