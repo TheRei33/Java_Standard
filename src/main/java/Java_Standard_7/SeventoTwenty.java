7d33039534bdf150919f610f9ce6d989ea64a66c
Jeong_hyunwoo <rlck51@naver.com>
2022-08-08 15:31:27 +0900

    자바의 정석 예제 7-20

---
+++ src/main/java/Java_Standard_7/SeventoTwenty.java
@@ -0,0 +1,32 @@
+package Java_Standard_7;
+
+public class SeventoTwenty {
+    public static void main(String[] args) {
+        Parent3 p = new Child3();
+        Child3 c = new Child3();
+
+        System.out.println("p.x = " + p.x);
+        p.method();
+        System.out.println();
+        System.out.println("c.x = " + c.x);
+        c.method();
+    }
+}
+
+class Parent3{
+    int x = 100;
+
+    void method() {
+        System.out.println("Parent Method");
+    }
+}
+
+class Child3 extends Parent3 {
+    int x = 200;
+
+    void method() {
+        System.out.println("x = " + x);
+        System.out.println("super.x = " + super.x);
+        System.out.println("this.x = " + this.x);
+    }
+}
