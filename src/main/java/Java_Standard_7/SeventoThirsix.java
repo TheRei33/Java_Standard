c6c7e50ed6ec3c5e395ba9b0d1d6527aa1525dce
Jeong_hyunwoo <rlck51@naver.com>
2022-08-24 09:22:01 +0900

    자바의 정석 예제 7-36

---
+++ src/main/java/Java_Standard_7/SeventoThirsix.java
@@ -0,0 +1,10 @@
+package Java_Standard_7;
+
+public class SeventoThirsix {
+    Object iv = new Object() { void method() {} };
+    static Object cv = new Object() { void method() {} };
+
+    void myMethod() {
+        Object lv = new Object() { void method() {} };
+    }
+}
