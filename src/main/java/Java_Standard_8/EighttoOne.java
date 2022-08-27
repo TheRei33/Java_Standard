0665446c36751df6b2c8b630d0ce4fd2342a5e6b
Jeong_hyunwoo <rlck51@naver.com>
2022-08-27 19:56:31 +0900

    자바의 정석 예제 8-1

---
+++ src/main/java/Java_Standard_8/EighttoOne.java
@@ -0,0 +1,16 @@
+package Java_Standard_8;
+
+public class EighttoOne {
+    public static void main(String[] args) {
+        try {
+            try { } catch (Exception e) { }
+        } catch (Exception e) {
+//            try { } catch (Exception e) { }
+        }
+
+        try {
+
+        } catch (Exception e) {
+        }
+    }
+}
