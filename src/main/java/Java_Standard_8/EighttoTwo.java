af9eaf67b3f2105c352e2f96ab70034538999211
Jeong_hyunwoo <rlck51@naver.com>
2022-08-28 14:22:36 +0900

    자바의 정석 예제 8-2

---
+++ src/main/java/Java_Standard_8/EighttoTwo.java
@@ -0,0 +1,13 @@
+package Java_Standard_8;
+
+public class EighttoTwo {
+    public static void main(String[] args) {
+        int number = 100;
+        int result = 0;
+
+        for(int i =0; i <10; i++ ) {
+            result = number / (int)(Math.random() * 10);
+            System.out.println(result);
+        }
+    }
+}
