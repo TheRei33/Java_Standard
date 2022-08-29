69657c9265df99b4241259eb5976260e326d4e57
Jeong_hyunwoo <rlck51@naver.com>
2022-08-29 21:59:26 +0900

    자바의 정석 예제 8-3

---
+++ src/main/java/Java_Standard_8/EighttoThree.java
@@ -0,0 +1,17 @@
+package Java_Standard_8;
+
+public class EighttoThree {
+    public static void main(String[] args) {
+        int number = 100;
+        int result = 0;
+
+        for(int i =0; i <10; i++ ) {
+            try {
+                result = number / (int)(Math.random() * 10);
+                System.out.println(result);
+            } catch(ArithmeticException e) {
+                System.out.println("0");
+            }
+        }
+    }
+}
