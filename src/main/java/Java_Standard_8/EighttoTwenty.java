8abb04ec73932644c6ee301eb7519965196bc4ed
Jeong_hyunwoo <rlck51@naver.com>
2022-09-16 09:59:56 +0900

    자바의 정석 예제 8-20

---
+++ src/main/java/Java_Standard_8/EighttoTwenty.java
@@ -0,0 +1,46 @@
+package Java_Standard_8;
+
+public class EighttoTwenty {
+    public static void main(String[] args) {
+        try (CloseableResource cr = new CloseableResource()) {
+            cr.exceptionWork(false);
+        } catch (WorkException e) {
+            e.printStackTrace();
+        } catch (CloseException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+
+        try (CloseableResource cr = new CloseableResource()) {
+            cr.exceptionWork(true);
+        } catch (WorkException e) {
+            e.printStackTrace();
+        } catch (CloseException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+class CloseableResource implements AutoCloseable {
+    public void exceptionWork(boolean exception) throws WorkException {
+        System.out.println("exceptionWork("+exception+")가 호출됨;");
+
+        if(exception)
+            throw new WorkException("WorkException발생!!!");
+    }
+
+    public void close() throws CloseException {
+        System.out.println("close()가 호출됨");
+        throw new CloseException("CloseException발생!!!");
+    }
+}
+
+class WorkException extends Exception {
+    WorkException(String msg) { super(msg); }
+}
+
+class CloseException extends Exception {
+    CloseException(String msg) { super(msg); }
+}
