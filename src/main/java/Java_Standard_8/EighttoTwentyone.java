d2f2e73b443656243ba494d87a6f65da7b92fef3
Jeong_hyunwoo <rlck51@naver.com>
2022-09-17 15:53:44 +0900

    자바의 정석 예제 8-21

---
+++ src/main/java/Java_Standard_8/EighttoTwentyone.java
@@ -0,0 +1,51 @@
+package Java_Standard_8;
+
+public class EighttoTwentyone {
+    public static void main(String[] args) {
+        try {
+            startInstall();
+            copyFiles();
+        } catch (SpaceException e) {
+            System.out.println("에러 메시지 : " + e.getMessage());
+            e.printStackTrace();
+            System.out.println("공간을 확보한 후에 다시 설치하시기 바랍니다.");
+        } catch (MemoryException me) {
+            System.out.println("에러 메시지 : " + me.getMessage());
+            me.printStackTrace();
+            System.gc();
+            System.out.println("다시 설치 시도하세요.");
+        } finally {
+            deleteTempFiles();
+        }
+    }
+
+    static void startInstall() throws SpaceException, MemoryException {
+        if(!enoughSpace())
+            throw new SpaceException("설치할 공간이 부족합니다.");
+        if(!enoughMemory())
+            throw new MemoryException("메모리가 부족합니다.");
+    }
+
+    static void copyFiles() {}
+    static void deleteTempFiles() { }
+
+    static boolean enoughSpace() {
+        return false;
+    }
+
+    static boolean enoughMemory() {
+        return true;
+    }
+}
+
+class SpaceException extends Exception {
+    SpaceException(String msg) {
+        super(msg);
+    }
+}
+
+class MemoryException extends Exception {
+    MemoryException(String msg) {
+        super(msg);
+    }
+}
