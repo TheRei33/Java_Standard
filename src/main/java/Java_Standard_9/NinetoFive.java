0c664cab50078c08ecc26edb67c9bcbcd650b294
Jeong_hyunwoo <rlck51@naver.com>
2022-09-25 19:04:13 +0900

    자바의 정석 예제 9-5

---
+++ src/main/java/Java_Standard_9/NinetoFive.java
@@ -0,0 +1,13 @@
+package Java_Standard_9;
+
+public class NinetoFive {
+    public static void main(String[] args) {
+        String str = new String("KOERA");
+        java.util.Date today = new java.util.Date();
+
+        System.out.println(str);
+        System.out.println(str.toString());
+        System.out.println(today);
+        System.out.println(today.toString());
+    }
+}
