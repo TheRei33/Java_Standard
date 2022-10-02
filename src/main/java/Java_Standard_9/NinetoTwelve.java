bd6c9f23c881315c907f321ee310319d57bd0f0f
Jeong_hyunwoo <rlck51@naver.com>
2022-10-02 18:20:28 +0900

    자바의 정석 예제 9-12

---
+++ src/main/java/Java_Standard_9/NinetoTwelve.java
@@ -0,0 +1,10 @@
+package Java_Standard_9;
+
+public class NinetoTwelve {
+    public static void main(String[] args) {
+        String s1 = "AAA";
+        String s2 = "AAA";
+        String s3 = "AAA";
+        String s4 = "BBB";
+    }
+}
