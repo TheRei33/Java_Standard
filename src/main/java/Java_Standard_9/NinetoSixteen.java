afdd781b18a1e4679a4c1f90133ade1d73daf104
Jeong_hyunwoo <rlck51@naver.com>
2022-10-06 08:28:08 +0900

    자바의 정석 예제 9-16

---
+++ src/main/java/Java_Standard_9/NinetoSixteen.java
@@ -0,0 +1,19 @@
+package Java_Standard_9;
+
+public class NinetoSixteen {
+    public static void main(String[] args) {
+        int iVal = 100;
+        String strVal = String.valueOf(iVal);
+
+        double dVal = 200.0;
+        String strVal2 = dVal + "";
+
+        double sum = Integer.parseInt("+"+strVal) + Double.parseDouble(strVal2);
+
+        double sum2 = Integer.valueOf(strVal) + Double.valueOf(strVal2);
+
+        System.out.println(String.join("", strVal,"+",strVal2,"=") + sum);
+        System.out.println(strVal + "+" + strVal2 + "=" + sum2);
+    }
+
+}
