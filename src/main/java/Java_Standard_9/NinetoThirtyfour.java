0efa546f3dae82b758e4077ec9fde403f6520988
Jeong_hyunwoo <rlck51@naver.com>
2022-10-25 18:23:57 +0900

    자바의 정석 예제 9-34

---
+++ src/main/java/Java_Standard_9/NinetoThirtyfour.java
@@ -0,0 +1,29 @@
+package Java_Standard_9;
+
+import java.util.regex.*;
+
+public class NinetoThirtyfour {
+    public static void main(String[] args) {
+        String source = "A broken hand works, but not a broken heart.";
+        String pattern = "broken";
+        StringBuffer sb = new StringBuffer();
+
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(source);
+        System.out.println("source : " + source);
+
+        int i = 0;
+
+        while(m.find()) {
+            System.out.println(++i + "번째 매칭 : " + m.start() + "-" + m.end());
+
+            m.appendReplacement(sb, "drunken");
+
+        }
+
+        m.appendTail(sb);
+        System.out.println("Replacement count : " + i);
+        System.out.println("result : " + sb.toString());
+    }
+
+}
