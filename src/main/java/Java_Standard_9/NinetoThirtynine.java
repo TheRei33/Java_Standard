093e87d535b648d21310c883f7687f57558e7207
Jeong_hyunwoo <rlck51@naver.com>
2022-10-30 15:24:53 +0900

    자바의 정석 예제 9-39

---
+++ src/main/java/Java_Standard_9/NinetoThirtynine.java
@@ -0,0 +1,15 @@
+package Java_Standard_9;
+
+import java.util.*;
+
+public class NinetoThirtynine {
+    public static void main(String[] args) {
+        String expression = "x=100*(200+300)/2";
+        StringTokenizer st = new StringTokenizer(expression, "+-*/=()", true);
+
+        while(st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+        }
+    }
+
+}
