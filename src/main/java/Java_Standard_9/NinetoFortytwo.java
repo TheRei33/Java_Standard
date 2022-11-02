ae3f50e491811d121494ad3f64a4137990cd409d
Jeong_hyunwoo <rlck51@naver.com>
2022-11-02 17:26:31 +0900

    자바의 정석 예제 9-42

---
+++ src/main/java/Java_Standard_9/NinetoFortytwo.java
@@ -0,0 +1,26 @@
+package Java_Standard_9;
+
+import java.util.*;
+
+public class NinetoFortytwo {
+    public static void main(String[] args) {
+        String data = "100,,,200,300";
+
+        String[] result = data.split(",");
+        StringTokenizer st = new StringTokenizer(data, ",");
+
+        for(int i =0; i < result.length ; i++) {
+            System.out.print(result[i] + "|");
+        }
+
+        System.out.println("개수 : " + result.length);
+
+        int i = 0;
+        for(;st.hasMoreTokens();i++) {
+            System.out.print(st.nextToken() + "|");
+        }
+
+        System.out.println("개수 : " + i);
+    }
+
+}
