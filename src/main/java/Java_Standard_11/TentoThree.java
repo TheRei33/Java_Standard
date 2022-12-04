7d998bd4a725a6efe728a7c417c4cd806fa41446
Jeong_hyunwoo <rlck51@naver.com>
2022-12-04 10:17:24 +0900

    자바의 정석 예제 11-3

---
+++ src/main/java/Java_Standard_11/TentoThree.java
@@ -0,0 +1,36 @@
+package Java_Standard_11;
+
+import java.util.Vector;
+
+public class TentoThree {
+    public static void main(String[] args) {
+        Vector v = new Vector(5);
+        v.add("1");
+        v.add("2");
+        v.add("3");
+        print(v);
+
+        v.trimToSize();
+        System.out.println("=== After trimToSize() ===");
+        print(v);
+
+        v.ensureCapacity(6);
+        System.out.println("=== After ensureCapacity(6) ===");
+        print(v);
+
+        v.setSize(7);
+        System.out.println("=== After setSize(7) ===");
+        print(v);
+
+        v.clear();
+        System.out.println("=== After clear() ===");
+        print(v);
+    }
+
+    public static void print(Vector v) {
+        System.out.println(v);
+        System.out.println("size : "+v.size());
+        System.out.println("capacity : "+v.capacity());
+    }
+
+}
