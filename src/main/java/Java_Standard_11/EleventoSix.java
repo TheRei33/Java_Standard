d5f2eeb16175733397038e5a6fa58889f0497d53
Jeong_hyunwoo <rlck51@naver.com>
2022-12-07 16:51:40 +0900

    자바의 정석 예제 11-6

---
+++ src/main/java/Java_Standard_11/EleventoSix.java
@@ -0,0 +1,30 @@
+package Java_Standard_11;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EleventoSix {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList(1000000);
+        LinkedList ll = new LinkedList();
+        add(al);
+        add(ll);
+
+        System.out.println("= 접근시간테스트 =");
+        System.out.println("ArrayList : "+access(al));
+        System.out.println("LinkedList : "+access(ll));
+    }
+
+    public static void add(List list) {
+        for(int i = 0; i < 1000000; i++) list.add(i+"");
+    }
+
+    public static long access(List list) {
+        long start = System.currentTimeMillis();
+        for(int i = 0;i<10000;i++) list.get(i);
+        long end = System.currentTimeMillis();
+        return end-start;
+    }
+
+}
