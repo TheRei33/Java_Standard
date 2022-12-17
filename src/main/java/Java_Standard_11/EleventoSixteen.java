d454cec43fa5efabd34676bb947c5c3f7d393bc9
Jeong_hyunwoo <rlck51@naver.com>
2022-12-17 16:38:56 +0900

    자바의 정석 예제 11-16

---
+++ src/main/java/Java_Standard_11/EleventoSixteen.java
@@ -0,0 +1,55 @@
+package Java_Standard_11;
+
+import java.util.Iterator;
+
+public class EleventoSixteen extends MyVector implements Iterator {
+    int cursor = 0;
+    int lastRet = -1;
+
+    public EleventoSixteen(int capacity) {
+        super(capacity);
+    }
+
+    public EleventoSixteen() {
+        this(10);
+    }
+
+    public String toString() {
+        String tmp = "";
+        Iterator it = Iterator();
+
+        for(int i = 0 ; it.hasNext() ; i++) {
+            if(i!=0) tmp+=", ";
+            tmp += it.next();
+        }
+
+        return "["+tmp+"]";
+    }
+
+    public Iterator Iterator() {
+        cursor = 0 ;
+        lastRet = -1;
+        return this;
+    }
+
+    public boolean hasNext() {
+        return cursor != size();
+    }
+
+    public Object next() {
+        Object next = get(cursor);
+        lastRet = cursor++;
+        return next;
+    }
+
+    public void remove() {
+
+        if(lastRet == -1) {
+            throw new IllegalStateException();
+        }else {
+            remove(lastRet);
+            cursor--;
+            lastRet = -1;
+        }
+    }
+}
