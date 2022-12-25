ee0c460ed862ba3f168f949cac810fd34aede848
Jeong_hyunwoo <rlck51@naver.com>
2022-12-25 13:34:30 +0900

    자바의 정석 예제 11-23

---
+++ src/main/java/Java_Standard_11/EleventoTwentythree.java
@@ -0,0 +1,31 @@
+package Java_Standard_11;
+
+import java.util.HashSet;
+
+public class EleventoTwentythree {
+    public static void main(String[] args) {
+        HashSet set = new HashSet();
+
+        set.add("abc");
+        set.add("abc");
+        set.add(new Person("David",10));
+        set.add(new Person("David",10));
+
+        System.out.println(set);
+    }
+}
+class Person{
+    String name;
+    int age;
+
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + ":"+age;
+    }
+}
+
