1e3420808ee25df96665495caced27c9868d198c
Jeong_hyunwoo <rlck51@naver.com>
2022-12-30 16:05:59 +0900

    자바의 정석 예제 11-28

---
+++ src/main/java/Java_Standard_11/EleventoTwentyeight.java
@@ -0,0 +1,10 @@
+package Java_Standard_11;
+
+public class EleventoTwentyeight {
+    public static void main(String[] args) {
+        char ch = ' ';
+
+        for(int i = 0 ; i < 95 ; i++)
+            System.out.print(ch++);
+    }
+}
