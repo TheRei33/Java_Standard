a14071c6377be714646c820ae4051de12262161d
Jeong_hyunwoo <rlck51@naver.com>
2023-01-11 15:57:50 +0900

    자바의 정석 예제 12-1

---
+++ src/main/java/Java_Standard_12/TwelvetoOne.java
@@ -0,0 +1,40 @@
+package Java_Standard_12;
+
+import java.util.ArrayList;
+
+class Fruit               { public String toString() { return "Fruit"; } }
+class Apple extends Fruit { public String toString() { return "Apple"; } }
+class Grape extends Fruit { public String toString() { return "Grape"; } }
+class Toy                 { public String toString() { return "Toy"; } }
+
+public class TwelvetoOne {
+    public static void main(String[] args) {
+        Box<Fruit> fruitBox = new Box<Fruit>();
+        Box<Apple> appleBox = new Box<Apple>();
+        Box<Toy> toyBox = new Box<Toy>();
+//        Box<Grape> grapeBox = new Box<Apple>();
+
+        fruitBox.add(new Fruit());
+        fruitBox.add(new Apple()); // oK. void add(Fruit item)
+
+        appleBox.add(new Apple());
+        appleBox.add(new Apple());
+//        appleBox.add(new Toy());
+
+        toyBox.add(new Toy());
+//        toyBox.add(new Apple());
+
+        System.out.println(fruitBox);
+        System.out.println(appleBox);
+        System.out.println(toyBox);
+
+    }
+}
+
+class Box<T>{
+    ArrayList<T> list = new ArrayList<T>();
+    void add(T item) { list.add(item); }
+    T get( int i ) { return list.get(i); }
+    int size() { return list.size(); }
+    public String toString() { return list.toString(); }
+}
