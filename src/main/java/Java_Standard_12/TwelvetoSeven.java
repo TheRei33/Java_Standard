0173acbc706912e7aee2076f639b71018f195dff
Jeong_hyunwoo <rlck51@naver.com>
2023-01-17 11:03:03 +0900

    자바의 정석 예제 12-7

---
+++ src/main/java/Java_Standard_12/TwelvetoSeven.java
@@ -0,0 +1,28 @@
+package Java_Standard_12;
+
+enum Transportation{
+    BUS(100) {int fare(int distance) {return distance*BASIC_FARE;}},
+    TRAIN(150) {int fare(int distance) {return distance*BASIC_FARE;}},
+    SHIP(100) {int fare(int distance) {return distance*BASIC_FARE;}},
+    AIRPLANE(300) {int fare(int distance) {return distance*BASIC_FARE;}};
+
+    protected final int BASIC_FARE;
+
+    Transportation(int basicFare){
+        BASIC_FARE = basicFare;
+    }
+
+    public int getBasicFare() {return BASIC_FARE;}
+
+    abstract int fare(int distance);
+}
+
+public class TwelvetoSeven {
+    public static void main(String[] args) {
+        System.out.println("bus fare = "+Transportation.BUS.fare(100));
+        System.out.println("train fare = "+Transportation.TRAIN.fare(100));
+        System.out.println("ship fare = "+Transportation.SHIP.fare(100));
+        System.out.println("airplane fare = "+Transportation.AIRPLANE.fare(100));
+
+    }
+}
