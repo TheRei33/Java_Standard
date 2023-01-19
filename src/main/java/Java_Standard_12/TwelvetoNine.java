3e63d8005d818271b2c4764233894aa34b950adb
Jeong_hyunwoo <rlck51@naver.com>
2023-01-19 11:06:39 +0900

    자바의 정석 예제 12-9

---
+++ src/main/java/Java_Standard_12/TwelvetoNine.java
@@ -0,0 +1,13 @@
+package Java_Standard_12;
+
+class Parent {
+    void parentMethod() {}
+}
+
+class Child extends Parent {
+    @Override
+    void parentmethod() {}
+}
+
+public class TwelvetoNine {
+}
