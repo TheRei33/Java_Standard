520045637c978eadf80d39e78134cc0fd61ce061
Jeong_hyunwoo <rlck51@naver.com>
2023-01-27 19:49:46 +0900

    자바의 정석 예제 13-4

---
+++ src/main/java/Java_Standard_13/ThirteentoFour.java
@@ -0,0 +1,17 @@
+package Java_Standard_13;
+
+public class ThirteentoFour {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        for(int i=0;i<300;i++)
+            System.out.printf("%s", new String("-"));
+
+        System.out.print("소요시간1:" + (System.currentTimeMillis() - startTime));
+
+        for(int i = 0; i<300;i++)
+            System.out.printf("%s", new String("|"));
+
+        System.out.print("소요시간2:" + (System.currentTimeMillis() - startTime));
+    }
+}
