d765a5ade97ddd59c58d24517969cd35ca889ef8
Jeong_hyunwoo <rlck51@naver.com>
2023-01-30 14:26:35 +0900

    자바의 정석 예제 13-7

---
+++ src/main/java/Java_Standard_13/ThirteentoSeven.java
@@ -0,0 +1,18 @@
+package Java_Standard_13;
+
+import javax.swing.*;
+
+public class ThirteentoSeven {
+    public static void main(String[] args) throws Exception {
+        String input = JOptionPane.showInputDialog("아무 값이나 입력하세요.");
+        System.out.println("입력하신 값은 "+ input + "입니다.");
+
+        for(int i=10; i> 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch(Exception e) {}
+        }
+    }
+}
+
