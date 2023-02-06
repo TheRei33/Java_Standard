b9b7ebbe91ec8c75ad517ee84e9b10f8eb62ed43
Jeong_hyunwoo <rlck51@naver.com>
2023-02-06 14:39:15 +0900

    자바의 정석 예제 13-14

---
+++ src/main/java/Java_Standard_13/ThirteentoFourteen.java
@@ -0,0 +1,27 @@
+package Java_Standard_13;
+
+public class ThirteentoFourteen {
+    public static void main(String[] args) {
+        ThreadEx14_1 th = new ThreadEx14_1();
+        th.start();
+
+        String input = JOptionPane.showInputDialog("아무 값이나 입력하세요.");
+        System.out.println("입력하신 값은 "+input+ "입니다.");
+        th.interrupt();
+        System.out.println("isInterrupted():"+th.isInterrupted());
+    }
+}
+
+class ThreadEx14_1 extends Thread {
+    public void run() {
+        int i =10;
+
+        while(i!=0 && !isInterrupted()) {
+            System.out.println(i--);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {}
+        }
+        System.out.println("카운터가 종료되었습니다.");
+    }
+}
