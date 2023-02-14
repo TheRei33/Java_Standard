a6de71b7513f6f8d7feacd0ad55e753da63150e4
Jeong_hyunwoo <rlck51@naver.com>
2023-02-14 09:58:32 +0900

    자바의 정석 예제 13-22

---
+++ src/main/java/Java_Standard_13/ThirteentoTwentytwo.java
@@ -0,0 +1,39 @@
+package Java_Standard_13;
+
+public class ThirteentoTwentytwo {
+    public static void main(String[] args) {
+        Runnable r = new RunnableEx22();
+        new Thread(r).start();
+        new Thread(r).start();
+
+    }
+}
+
+class Account {
+    private int balance = 1000;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public synchronized void withdraw(int money) {
+        if(balance >= money) {
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e) {}
+            balance -= money;
+        }
+    }
+}
+
+class RunnableEx22 implements Runnable {
+    Account acc = new Account();
+
+    public void run() {
+        while(acc.getBalance() > 0) {
+            int money = (int)(Math.random() * 3 + 1) * 100;
+            acc.withdraw(money);
+            System.out.println("balance: " + acc.getBalance());
+        }
+    }
+}
