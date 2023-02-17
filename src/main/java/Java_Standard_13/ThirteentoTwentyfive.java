64e35dbd2aad509b78df5cc499762747d6d23757
Jeong_hyunwoo <rlck51@naver.com>
2023-02-17 16:19:03 +0900

    자바의 정석 예제 13-25

---
+++ src/main/java/Java_Standard_13/ThirteentoTwentyfive.java
@@ -0,0 +1,105 @@
+package Java_Standard_13;
+
+import java.util.ArrayList;
+
+class Customer implements Runnable {
+    private Table table;
+    private String food;
+
+    Customer(Table table, String food) {
+        this.table = table;
+        this.food = food;
+    }
+
+    public void run() {
+        while(true) {
+            try { Thread.sleep(100);}catch(InterruptedException e){}
+            String name = Thread.currentThread().getName();
+
+            table.remove(food);
+            System.out.println(name + " ate a " + food);
+
+        }
+    }
+    boolean eatFood() {return table.remove(food);}
+}
+
+class Cook implements Runnable {
+    private Table table;
+
+    Cook(Table table) {this.table = table;}
+
+    public void run() {
+        while(true) {
+            int idx = (int)(Math.random()*table.dishNum());
+            table.add(table.dishNames[idx]);
+
+            try { Thread.sleep(10); } catch(InterruptedException e) {}
+        }
+    }
+}
+
+class Table {
+    String[] dishNames = {"donut","dount","burger"};
+    final int MAX_FOOD = 6;
+
+    private ArrayList<String> dishes = new ArrayList<>();
+
+    public synchronized void add(String dish) {
+        while(dishes.size() >= MAX_FOOD) {
+            String name = Thread.currentThread().getName();
+            System.out.println(name+" is waiting.");
+            try{
+                wait();
+                Thread.sleep(500);
+            } catch(InterruptedException e) {}
+        }
+        dishes.add(dish);
+        notify();
+        System.out.println("Dishes: " + dishes.toString());
+    }
+
+    public boolean remove(String dishName) {
+        synchronized (this) {
+            String name = Thread.currentThread().getName();
+
+            while(dishes.size() ==0) {
+                System.out.println(name+" is waiting.");
+                try{
+                    wait();
+                    Thread.sleep(500);
+                } catch(InterruptedException e) {}
+            }
+
+            while(true) {
+                for(int i =0; i <dishes.size();i++) {
+                    if(dishName.equals(dishes.get(i))) {
+                        dishes.remove(i);
+                        notify();
+                        return false;
+                    }
+                }
+
+                try{
+                    wait();
+                    Thread.sleep(500);
+                } catch(InterruptedException e) {}
+            }
+        }
+    }
+
+    public int dishNum() { return dishNames.length; }
+}
+
+public class ThirteentoTwentyfive {
+    public static void main(String[] args) throws Exception {
+        Table table = new Table();
+
+        new Thread(new Cook(table), "COOK1").start();
+        new Thread(new Customer(table, "dount"), "CUST1").start();
+        new Thread(new Customer(table, "burger"), "CUST2").start();
+
+        Thread.sleep(2000);
+        System.exit(0);
+    }
+}
