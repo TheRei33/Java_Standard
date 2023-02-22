79f8f1c92d7e8c048e4e604bd53563f97f6fd2fa
Jeong_hyunwoo <rlck51@naver.com>
2023-02-22 09:40:46 +0900

    자바의 정석 예제 14-3

---
+++ src/main/java/Java_Stardard_14/FourteentoThree.java
@@ -0,0 +1,36 @@
+package Java_Stardard_14;
+
+@FunctionalInterface
+interface MyFunction {
+    void myMethod();
+}
+
+
+class Outer {
+    int val = 10;
+
+    class Inner {
+        int val =20;
+
+        void method(int i) {
+            int val =30;
+
+            MyFunction f = () -> {
+                System.out.println("i: " + i);
+                System.out.println("val: " + val);
+                System.out.println("this.val: " + ++this.val);
+                System.out.println("Outer.this.val: " + ++Outer.this.val);
+            };
+
+            f.myMethod();
+        }
+    }
+}
+
+public class FourteentoThree {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.new Inner();
+        inner.method(100);
+    }
+}
