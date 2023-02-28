d686eb737ad85350eef6d5c089e8b438b919ef31
Jeong_hyunwoo <rlck51@naver.com>
2023-02-28 18:02:25 +0900

    자바의 정석 예제 14-9

---
+++ src/main/java/Java_Stardard_14/FourteentoNine.java
@@ -0,0 +1,28 @@
+package Java_Stardard_14;
+
+import java.io.File;
+import java.util.stream.Stream;
+
+public class FourteentoNine {
+    public static void main(String[] args) {
+        File[] fileArr = { new File("Ex1.java"), new File("Ex1.bak"),
+                new File("Ex2.java"), new File("Ex1"), new File("Ex1.txt")
+        };
+
+        Stream<File> fileStream = Stream.of(fileArr);
+
+        Stream<String> filenameStream = fileStream.map(File::getName);
+        filenameStream.forEach(System.out::println);
+
+        fileStream = Stream.of(fileArr);
+
+        fileStream.map(File::getName)
+                .filter(s -> s.indexOf('.')!=-1)
+                .map(s -> s.substring(s.indexOf('.')+1))
+                .map(String::toUpperCase)
+                .distinct()
+                .forEach(System.out::print);
+
+        System.out.println();
+    }
+}
