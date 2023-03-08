b94614236ca296dec996cdbff2f918106fc059f5
Jeong_hyunwoo <rlck51@naver.com>
2023-03-08 11:07:58 +0900

    자바의 정석 예제 14-17

---
+++ src/main/java/Java_Stardard_14/FourteentoSeventeen.java
@@ -0,0 +1,49 @@
+package Java_Stardard_14;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class FourteentoSeventeen {
+    public static void main(String[] args) {
+        String[] strArr = {"aaa", "bbb", "ccc"};
+        Stream<String> strStream = Stream.of(strArr);
+
+        String result = strStream.collect(new ConcatCollector());
+
+        System.out.println(Arrays.toString(strArr));
+        System.out.println("result = "+ result);
+
+    }
+}
+
+class ConcatCollector implements Collector<String, StringBuilder, String> {
+    @Override
+    public Supplier<StringBuilder> supplier() {
+        return () -> new StringBuilder();
+//        return StringBuilder::new;
+    }
+
+    @Override
+    public BiConsumer<StringBuilder, String> accumulator() {
+        return (sb, s) -> sb.append(s);
+//        return StringBuilder::new;
+    }
+
+    @Override
+    public Function<StringBuilder, String> finisher() {
+        return sb -> sb.toString();
+//        return StringBuilder::toString;
+    }
+
+    @Override
+    public BinaryOperator<StringBuilder> combiner() {
+        return (sb, sb2) -> sb.append(sb2);
+//        return StringBuilder::append;
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.emptySet();
+    }
+}
