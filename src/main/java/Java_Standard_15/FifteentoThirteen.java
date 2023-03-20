a59edf28cbc1d6d55a8397f1035550e0cf9939ec
Jeong_hyunwoo <rlck51@naver.com>
2023-03-21 08:38:30 +0900

    자바의 정석 예제 15-13

---
+++ src/main/java/Java_Standard_15/FifteentoThirteen.java
@@ -0,0 +1,26 @@
+package Java_Standard_15;
+
+import java.io.*;
+import java.util.*;
+
+public class FifteentoThirteen {
+    public static void main(String[] args) {
+        int sum = 0;
+        int score = 0;
+
+        try {
+            FileInputStream fis = new FileInputStream("score.dat");
+            DataInputStream dis = new DataInputStream(fis);
+
+            while(true) {
+                score = dis.readInt();
+                System.out.println(score);
+                sum += score;
+            }
+        } catch (EOFException e) {
+            System.out.println("점수의 총합은 " + sum + "입니다.");
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        }
+    }
+}
