e09d28f62a1e1144cc60bbc2cdaf91c17b352697
Jeong_hyunwoo <rlck51@naver.com>
2023-03-31 05:53:53 +0900

    자바의 정석 예제 15-23

---
+++ src/main/java/Java_Standard_15/FifteentoTwentythree.java
@@ -0,0 +1,8 @@
+package Java_Standard_15;
+
+public class FifteentoTwentythree {
+    public static void main(String[] args) {
+        System.out.println("out : Hello World!");
+        System.out.println("err : Hello World!");
+    }
+}
