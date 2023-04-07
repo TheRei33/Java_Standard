da006a01e3266ae415006e0f09ab3d301a82f6e2
Jeong_hyunwoo <rlck51@naver.com>
2023-04-08 07:02:08 +0900

    자바의 정석 예제 15-30

---
+++ src/main/java/Java_Standard_15/FifteentoThirty.java
@@ -0,0 +1,60 @@
+package Java_Standard_15;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FifteentoThirty {
+    static int totalFiles = 0;
+    static int totalDirs = 0;
+
+    public static void main(String[] args) {
+        if(args.length != 1) {
+            System.out.println("USAGE : java jsp15_30 DIRECTORY");
+            System.exit(0);
+        }
+
+        File dir = new File(args[0]);
+
+        if(!dir.exists() || !dir.isDirectory()) {
+            System.out.println("유효하지 않은 디렉토리입니다.");
+            System.exit(0);
+        }
+
+        printFileList(dir);
+
+        System.out.println();
+        System.out.println("총 " + totalFiles + "개의 파일");
+        System.out.println("총 " + totalDirs + "개의 디렉토리");
+    }
+
+    public static void printFileList(File dir) {
+        System.out.println(dir.getAbsolutePath() + " 디렉토리");
+        File[] files = dir.listFiles();
+
+        ArrayList subDir = new ArrayList();
+
+        for(int i=0; i < files.length;i++) {
+            String filename = files[i].getName();
+
+            if(files[i].isDirectory()) {
+                filename = "["+filename+"]";
+                subDir.add(i+"");
+            }
+            System.out.println(filename);
+        }
+
+        int dirNum = subDir.size();
+        int fileNum = files.length - dirNum;
+
+        totalFiles += fileNum;
+        totalDirs += dirNum;
+
+        System.out.println(fileNum + "개의 파일, " + dirNum + "개의 디렉토리");
+        System.out.println();
+
+        for(int i=0; i < subDir.size();i++) {
+            int index = Integer.parseInt((String)subDir.get(i));
+            printFileList(files[index]);
+        }
+    }
+}
