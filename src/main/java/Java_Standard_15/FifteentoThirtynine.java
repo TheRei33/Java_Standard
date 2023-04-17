f84b14f350ab789e880b35ed24cb3998e5bc67fd
Jeong_hyunwoo <rlck51@naver.com>
2023-04-18 05:53:38 +0900

    자바의 정석 예제 15-39

--- src/main/java/Java_Standard_15/FifteentoThirtynine.java
+++ src/main/java/Java_Standard_15/FifteentoThirtynine.java
@@ -5,11 +5,11 @@ public class FifteentoThirtynine implements java.io.Serializable {
     String password;
     int age;
 
-    public UserInfo() {
+    public FifteentoThirtynine() {
         this("Unknown", "1111",0);
     }
 
-    public UserInfo(String name, String password, int age) {
+    public FifteentoThirtynine(String name, String password, int age) {
         this.name = name;
         this.password = password;
         this.age = age;
